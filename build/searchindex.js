337f50d6b421a30e7a0250d381180b980689342d
Brad Miller <bonelake@gmail.com>
2014-02-17 10:13:37 -0600

    remove pyc add gitignore

--- build/searchindex.js
+++ build/searchindex.js
@@ -1,1 +1,1 @@
-Search.setIndex({envversion:42,terms:{},objtypes:{},objnames:{},filenames:["index"],titles:["This Is A New Project"],objects:{},titleterms:{anoth:0,section:0,random:0,project:0,thi:0,"new":0}})
+Search.setIndex({objects:{},terms:{anoth:0,section:0,random:0,project:0,thi:0,"new":0},objtypes:{},titles:["This Is A New Project"],objnames:{},filenames:["index"]})
