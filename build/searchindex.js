016995fd52049c8a775e5ea6203be42ce7cbd718
Brad Miller <bonelake@gmail.com>
2014-02-17 09:16:05 -0600

    first commit of template

---
+++ build/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({envversion:42,terms:{},objtypes:{},objnames:{},filenames:["index"],titles:["This Is A New Project"],objects:{},titleterms:{anoth:0,section:0,random:0,project:0,thi:0,"new":0}})
